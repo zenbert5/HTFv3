a61e957eaad1246e144a6b1814de8450593a9efc
Robert Bridgeman <rob.bridgeman@gmail.com>
2018-11-13 12:19:45 -0800

    initial

---
+++ Friendsgiving/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friendsgiving
+//
+//  Created by Robert Bridgeman on 11/13/18.
+//  Copyright © 2018 Robert Bridgeman. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
