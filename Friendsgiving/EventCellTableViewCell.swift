5162bcefb90936af0db873e548dc3862981e50f5
Robert Bridgeman <rob.bridgeman@gmail.com>
2018-11-13 16:58:28 -0800

    eventsTVC and cust cell

---
+++ Friendsgiving/EventCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EventCellTableViewCell.swift
+//  Friendsgiving
+//
+//  Created by Robert Bridgeman on 11/13/18.
+//  Copyright © 2018 Robert Bridgeman. All rights reserved.
+//
+
+import UIKit
+
+class EventCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventHost: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
