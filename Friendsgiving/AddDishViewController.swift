94a3e8cee032a74a1d06121f6bfa243dd6b2d761
Robert Bridgeman <rob.bridgeman@gmail.com>
2018-11-14 10:16:19 -0800

    addDish vc

---
+++ Friendsgiving/AddDishViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddDishViewController.swift
+//  Friendsgiving
+//
+//  Created by Robert Bridgeman on 11/14/18.
+//  Copyright © 2018 Robert Bridgeman. All rights reserved.
+//
+
+import UIKit
+
+class AddDishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
